1607581d453ddf5385ca394fcd9ceb65b8698828
GlassOnTin <GlassOnTin@users.noreply.github.com>
2026-02-22 23:53:35 +0000

    Bump version to 0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "sh.haven.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
